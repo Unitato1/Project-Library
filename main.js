54bcb89eadb0cec8c47dc371c7cf212e787be1ec
Unitato1 <robertsvihla@gmail.com.github.com>
2023-04-18 20:56:41 +0200

    Form funcitioning how it should + some styling changes

--- main.js
+++ main.js
@@ -1,44 +1,79 @@
-const book = (author, title, pages, read) => {
-  return { author, title, pages, read };
+const bookFactory = (title, author, pages, read) => {
+  return { title, author, pages, read };
 };
 
-const bookself = (() => {
-  const create_bookshelf = () => {
+const bookshelf = (() => {
+  const book_container = document.querySelector("#book-container");
+  const create_bookshelf = (books) => {
+    book_container.replaceChildren();
+    let curr_book;
+    let title;
+    let author;
+    let pages;
+    let read;
+    for (book of books) {
+      title = document.createElement("p");
+      title.innerHTML = book.title;
+      author = document.createElement("p");
+      author.innerHTML = book.author;
+      pages = document.createElement("p");
+      pages.innerHTML = book.pages;
+      read = document.createElement("button");
+      read.addEventListener();
+      read.innerHTML = book.read === true ? "Read" : "Not read";
+
+      curr_book = document.createElement("div");
+      curr_book.className = "book";
+      curr_book.append(title, author, pages, read);
+      book_container.appendChild(curr_book);
+    }
     return;
   };
+  return { create_bookshelf };
 })();
 
 const main = (() => {
   const form = document.querySelector("#book-form");
   const add_book = document.querySelector("#book-add");
-  const main_elements = [
-    document.querySelector("header"),
-    document.querySelector("footer"),
-    document.querySelector("main"),
-  ];
-  let running = false;
-  add_book.addEventListener("click", (e) => {
-    for (element of main_elements) {
-      element.className =
-        element.className === "hidden_behind"
-          ? "showed_behind"
-          : "hidden_behind";
-    }
+  const submit = document.querySelector("#submit");
+  const main_con = document.querySelector("#main_con");
+  const title = document.querySelector("#title");
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+  const read = document.querySelector("#read");
+  let books = [];
+
+  const reset_form = () => {
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    read.checked = false;
+  };
+
+  const toggle_form = () => {
+    main_con.className =
+      main_con.className === "hidden_behind"
+        ? "showed_behind"
+        : "hidden_behind";
+
     form.className = form.className === "hidden" ? "show" : "hidden";
-    running = true;
-    document.addEventListener("click", (e) => {
-      if (!form.contains(e.target) && running === false) {
-        console.log("hello");
-        for (element of main_elements) {
-          element.className =
-            element.className === "hidden_behind"
-              ? "showed_behind"
-              : "hidden_behind";
-          form.className = form.className === "hidden" ? "show" : "hidden";
-          running = true;
-        }
-      }
-      running = false;
-    });
+  };
+
+  add_book.addEventListener("click", (e) => {
+    toggle_form();
+    if (!form.contains(e.target)) {
+      console.log("hello");
+    }
+  });
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    books.push(
+      bookFactory(title.value, author.value, pages.value, read.checked)
+    );
+
+    reset_form();
+    toggle_form();
+    bookshelf.create_bookshelf(books);
+    console.table(books);
   });
 })();
